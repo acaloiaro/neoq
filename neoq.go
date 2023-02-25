2897f9c4b157240a95e211f60f623e24a0ee8cc4
Juan C. Müller <jcmuller@gmail.com>
2023-02-25 17:04:08 -0500

    Move to doc

--- neoq.go
+++ neoq.go
@@ -1,8 +1,3 @@
-// Package neoq provides background job processing for Go applications.
-//
-// Neoq's goal is to minimize the infrastructure necessary to add background job processing to Go applications. It does so by implementing queue durability with modular backends, rather than introducing a strict dependency on a particular backend such as Redis.
-//
-// An in-memory and Postgres backend are provided out of the box.
 package neoq
 
 // TODO factor out the following dependencies
