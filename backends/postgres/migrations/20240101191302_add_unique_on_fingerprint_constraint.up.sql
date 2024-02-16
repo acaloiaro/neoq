a971e9c9009449c7db79f9aaa9048bbd8c160378
Phil Constantinou <constantinou@gmail.com>
2024-02-16 00:44:04 -0800

    Drop duplicate index

--- backends/postgres/migrations/20240101191302_add_unique_on_fingerprint_constraint.up.sql
+++ backends/postgres/migrations/20240101191302_add_unique_on_fingerprint_constraint.up.sql
@@ -1,3 +1,2 @@
-CREATE UNIQUE INDEX IF NOT EXISTS neoq_jobs_fingerprint_run_unique_idx ON neoq_jobs (queue, fingerprint, status, ran_at);
 DROP INDEX neoq_jobs_fingerprint_unique_idx;
-CREATE UNIQUE INDEX IF NOT EXISTS neoq_jobs_fingerprint_unique_idx ON neoq_jobs (queue, fingerprint, status) WHERE NOT (status = 'processed');
+CREATE UNIQUE INDEX IF NOT EXISTS neoq_jobs_fingerprint_run_unique_idx ON neoq_jobs (queue, status, fingerprint, ran_at);
